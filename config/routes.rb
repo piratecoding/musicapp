b3873784d7f98f66da5286cdc023bb7f9eba8c60
zlatanowsky <amaratc@gmail.com>
2017-09-28 21:06:47 +0200

    Add admin to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get '/about', 	to: 'static_pages#about'
+  get '/help',		to: 'static_pages#help'
+
+
 end
