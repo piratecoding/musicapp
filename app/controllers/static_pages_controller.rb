17dd3b3b40dd6911a3a91b626863f281265b1d28
zlatanowsky <amaratc@gmail.com>
2017-11-14 22:29:57 +0100

    Fixing bootstrap

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,4 +9,7 @@ class StaticPagesController < ApplicationController
 	def help		
 	end
 
+	def contact		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
   resources :albums
   resources :songs
   root 'static_pages#home'
 
   get '/about', 	to: 'static_pages#about'
   get '/help',		to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
 
   resources :users
 end
